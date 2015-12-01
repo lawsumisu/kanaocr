e9c70dc758d78075b672a3834261a5c6c25c65cd
Kimberly Toy <tokutei12@gmail.com>
2015-11-30 19:51:28 -0500

    segmentation

--- ocr.m
+++ ocr.m
@@ -31,7 +31,31 @@ if BW_row_sum(length(BW_row_sum))>0
 end
 
 %per row: vertical scan, find left/right of each character (any column with a single black pixel)
-BW_col_sum = sum(inv_BW);
+col_start = {};
+col_end = {};
+
+for m=1:length(row_start)
+    % retrieve section of inverse image that is the row
+    BW_col = inv_BW(row_start(m):row_end(m),:);
+    BW_col_sum = sum(BW_col);
+    flag = 0;
+    col_start{m} = [];
+    col_end{m} = [];
+    for n=1:length(BW_col_sum)
+        if flag==0 && BW_col_sum(n)~=0
+            col_start{m} = [col_start{m} n];
+            flag = 1;
+        elseif flag~=0 && BW_col_sum(n)==0
+            col_end{m} = [col_end{m} n-1];
+            flag = 0;
+        end
+    end
+    % edge case, if row ends at bottom of image, need to save final row_end
+    % index
+    if BW_col_sum(length(BW_col_sum))>0
+        col_end{m} = [col_end{m} length(BW_col_sum)];
+    end
+end
 
 %Character recognition
 %
